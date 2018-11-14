1ed47733a7ee3ca1ad405f37b732e73949c3bb4e
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-14 14:21:29 -0500

    Find a book

--- traversal-drills.js
+++ traversal-drills.js
@@ -54,3 +54,48 @@ function preOrder(tree) {
 }
 
 console.log(preOrder(binarySearchTree));
+
+const library = [
+  { author: 'Cowlishaw, Mike', dewey: '005.133', title: 'The REXX Language' },
+  { author: 'Sams', dewey: '005.133', title: 'Teach Yourself C++ In 21 Days' },
+  { author: 'Stroustrup., Bjarne', dewey: '005.133', title: 'The C++ Programming Language' },
+  { author: 'Crockford, Douglas', dewey: '005.2762', title: 'JavaScript: The Good Parts' },
+  { author: 'Flanagan, David', dewey: '005.2762', title: 'JavaScript: The Definitive Guide' },
+  { author: 'Schmidt, Meinhard', dewey: '005.44684', title: 'Windows Vista for Dummies' },
+  { author: 'Zondervan', dewey: '220.52081', title: 'NIV Study Bible' },
+  { author:'Humphries, Russell, Dr.', dewey: '231.7652', title: 'Starlight and Time' },
+  { author: 'Jane, Frederick Thomas', dewey: '623.82509051', title: 'Jane\'s Fighting Ships' },
+  { author: 'Norris, Chuck', dewey: '796.8092', title: 'The Official Chuck Norris Fact Book' }
+];
+
+// Input ->'005.133', 'Teach Yourself C++ In 21 Days'
+// Output -> Book
+
+// Find all books with the dewey
+// look at the subject (005)
+// look at the decimal (.133)
+// look at the title
+
+function findBook(libary, dewey, title) {
+  const subjectBooks = [];
+  libary.forEach(book => {
+    if (book.dewey.split('.')[0] === dewey.split('.')[0]) {
+      subjectBooks.push(book);
+    }
+  });
+
+  const subCategoryBooks = [];
+  subjectBooks.forEach(book => {
+    if (book.dewey.split('.')[1] === dewey.split('.')[1]) {
+      subCategoryBooks.push(book);
+    }
+  });
+
+  for (let i = 0; i< subCategoryBooks.length; i++) {
+    if (subCategoryBooks[i].title === title) {
+      return subCategoryBooks[i];
+    }
+  }
+}
+
+console.log(findBook(library, '005.133', 'Teach Yourself C++ In 21 Days'));
