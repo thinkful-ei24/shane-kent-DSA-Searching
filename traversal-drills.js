fce96c472dabf3f8b33f8c5cdaa632f37709830a
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-14 12:46:04 -0500

    Finish preorder

--- traversal-drills.js
+++ traversal-drills.js
@@ -1,37 +1,56 @@
 const BST = require('./BST');
 const binarySearchTree = new BST();
 
-const data = [25,15,50,10,24,35,70,4,12,18,31,44,66,90,22];
+const data = [25, 15, 50, 10, 24, 35, 70, 4, 12, 18, 31, 44, 66, 90, 22];
 
 data.forEach(item => binarySearchTree.insert(item, item));
 
 function inOrder(tree) {
   // L recursivly -> process root-> R recursivly
 
-  if(tree.left !== null) {
+  if (tree.left !== null) {
     inOrder(tree.left);
   }
-  console.log(tree.value)
-  if(tree.right !== null) {
+  console.log(tree.value);
+  if (tree.right !== null) {
     inOrder(tree.right);
   }
-  if(!tree.left && !tree.right) {
-    return
+  if (!tree.left && !tree.right) {
+    return;
   }
 }
 
 //console.log(inOrder(binarySearchTree));
 
 function postOrder(tree) {
-  // L recursivly -> process root-> R recursivly
+  // L recursivly -> R recursivly -> process root
 
-  if(tree.left) {
+  if (tree.left) {
     postOrder(tree.left);
   }
   if (tree.right) {
     postOrder(tree.right);
   }
-  console.log(tree.value)
+  console.log(tree.value);
 }
 
 console.log(postOrder(binarySearchTree));
+
+
+function preOrder(tree) {
+  // process root-> L recursivly -> R recursivly
+
+  console.log(tree.value);
+  if (!tree.left && !tree.right) {
+    return;
+  }
+  if (tree.left) {
+    preOrder(tree.left);
+  }
+  if (tree.right) {
+    preOrder(tree.right);
+  }
+
+}
+
+console.log(preOrder(binarySearchTree));
